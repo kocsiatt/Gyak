f939b046039311ca3795818615886e5b9007851e
kocsiatt <kocsiatt@gmail.com>
2025-10-14 12:10:50 +0200

    first project with venv and hello

--- Lib/Gyak.py
+++ Lib/Gyak.py
@@ -1,1 +1,15 @@
-print("Helló Attila!!")
+def name(name):
+
+    if not name:
+        return "Nem adtál meg nevet"
+    else:
+        return "Szia, " + name
+
+
+
+
+print(name(input("Kérlek add meg a neved!: ")))
+
+
+
+
