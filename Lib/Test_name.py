1f197405ef3e1a674a5298f00745b1f1183b8f4b
kocsiatt <kocsiatt@gmail.com>
2025-11-05 17:40:54 +0100

    First ReadMe to GitHub

--- Lib/Test_name.py
+++ Lib/Test_name.py
@@ -1,5 +1,5 @@
 import pytest
-"""from Gyak import greet"""
+from Gyak import greet
 from Gyak import check_password_strength
 
 
@@ -7,27 +7,27 @@ from Gyak import check_password_strength
 
 
 @pytest.mark.parametrize(
-    "imp, expected",
+    "inp, expected",
     [("abc", "Gyenge jelszó: túl rövid."),
         ("ABCDEFGHIJKL", "Gyenge jelszó: nincs benne kisbetű."),
         ("abcdefghijkl", "Gyenge jelszó: nincs nagybetű."),
         ("Abcdefghijkl", "Gyenge jelszó: nincs benne szám.")]
 )
 
-def check_password_strength(inp, expected):
+def test_check_password_strength(inp, expected):
     assert check_password_strength(inp) == expected
 
 
 
-    """@pytest.mark.parametrize(
-        "inp, expected",
-        [("", "Nem adtál meg nevet!"),
-            ("Attila", "Szia, Attila!"),
-            ("   Petra   ", "Szia, Petra!"),
-            ("  Ágota", "Szia, Ágota!"),
-         ("    ", "Nem adtál meg nevet!")]
-    )
+"""@pytest.mark.parametrize(
+    "inp, expected",
+     [("", "Nem adtál meg nevet!"),
+        ("Attila", "Szia, Attila!"),
+        ("   Petra   ", "Szia, Petra!"),
+        ("  Ágota", "Szia, Ágota!"),
+        ("    ", "Nem adtál meg nevet!")]
+)"""
 
 
-    def test_greet_variants(inp, expected):
-        assert greet(inp) == expected"""
+"""def test_greet_variants(inp, expected):
+    assert greet(inp) == expected"""
